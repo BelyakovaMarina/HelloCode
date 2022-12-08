4ebe8ee8427f78dbf27382a40eb0cd762ddd00bb
Marina <m_bogatyrenko@mail.ru>
2022-12-08 16:23:41 +0600

    Example 013 Recursion Algorithm

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -149,16 +149,58 @@ int Factorial(int n)
 Console.WriteLine(Factorial(3)); // 1 * 2 * 3 = 6
 */
 
+//ИЛИ
+
+/*
 double Factorial(int n)
 {
- // 1! = 1
- // 0! = 1
- if(n == 1) return 1;
- else return n * Factorial(n-1);
+    // 1! = 1
+    // 0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
 }
+
 for (int i = 1; i < 40; i++)
 {
- Console.WriteLine($"{i}! = {Factorial(i)}");
+    Console.WriteLine($"{i}! = {Factorial(i)}");
 }
+*/
+
+
+
+//Еще про Фибоначчи
+
+
+//f(1)=1
+//f(2)=1
+//f(n)=f(n-1)+f(n-2)
+
+/*
+int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for(int i = 1; i < 10; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+} 
+*/
+
+//ИЛИ
+
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for(int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+} 
+
 
 
